c8c1e2c346be39359702a368513217383f943f86
phob0s <spam@phob0s.pl>
2017-02-12 22:40:36 +0100

    Modify listen to have compilable code

--- internal/cmd/listen.go
+++ internal/cmd/listen.go
@@ -1,13 +1,14 @@
 package cmd
 
 import (
+	"log/syslog"
 	"os"
 	"time"
 
 	"github.com/alphasoc/namescore/internal/asoc"
 	"github.com/alphasoc/namescore/internal/config"
 	"github.com/alphasoc/namescore/internal/dns"
-	"github.com/alphasoc/namescore/internal/utils"
+	log "github.com/inconshreveable/log15"
 	"github.com/spf13/cobra"
 )
 
@@ -26,12 +27,21 @@ func init() {
 }
 
 func listen(cmd *cobra.Command, args []string) {
-	log := utils.Newlog()
-	defer log.Close()
+	logger := log.New()
+	if sysloghandler, err := log.SyslogHandler(syslog.LOG_USER|syslog.LOG_ERR, "namescore/listen", log.TerminalFormat()); err != nil {
+		logger.SetHandler(log.DiscardHandler())
+	} else {
+		logger.SetHandler(sysloghandler)
+	}
 
 	cfg := config.Get()
 	if err := cfg.ReadFromFile(); err != nil {
-		log.Warning("Failed to read config: " + err.Error())
+		logger.Warn("Failed to read config", "err", err)
+		os.Exit(1)
+	}
+
+	if cfg.APIKey == "" {
+		logger.Warn("API key not set.")
 		os.Exit(1)
 	}
 
@@ -40,16 +50,16 @@ func listen(cmd *cobra.Command, args []string) {
 
 	s, err := dns.Start(cfg.NetworkInterface)
 	if err != nil {
-		log.Warning("Failed to start sniffer " + err.Error())
+		logger.Warn("Failed to start sniffer", "err", err)
 		os.Exit(1)
 	}
-	log.Info("namescore daemon started")
+	logger.Info("namescore daemon started")
 
 	//gorutine for event retrieving
 	go func() {
 		alertStore, err := asoc.OpenAlerts(cfg.GetAlertFilePath())
 		if err != nil {
-			log.Warning("Failed to open alert file: " + err.Error())
+			logger.Warn("Failed to open alert file", "err", err)
 			return
 		}
 		defer alertStore.Close()
@@ -65,16 +75,16 @@ func listen(cmd *cobra.Command, args []string) {
 		}
 	}()
 
-	send := func(e []asoc.Entry) {
+	send := func(e []*asoc.Entry) {
 		if len(e) == 0 {
 			return
 		}
-		if err := client.Queries(asoc.QueriesReq{Data: e}); err != nil {
+		if _, err := client.Queries(&asoc.QueriesReq{Data: e}); err != nil {
 			//errorhandling
 		}
 	}
 
-	var container []asoc.Entry
+	var container []*asoc.Entry
 	for {
 		dns := s.GetDNS()
 		container = append(container, dns...)
