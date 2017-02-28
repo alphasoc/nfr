d7b4b6484ed0cfeba516f367361f1a6284843686
phob0s <git@phob0s.pl>
2017-02-28 21:53:51 +0100

    Fixed appearing empty line in alerts file.

--- asoc/alerts.go
+++ asoc/alerts.go
@@ -8,6 +8,10 @@ import (
 // StoreAlerts appends alerts collected from Events api call
 // If alerts are stored for a first time, file in path is created.
 func StoreAlerts(path string, alerts []string) (err error) {
+	if len(alerts) == 0 {
+		return
+	}
+
 	file, errOpen := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if errOpen != nil {
 		return errOpen
