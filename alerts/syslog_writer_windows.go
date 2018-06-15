c01730daccb6f0c48cc94360aa45ce842073101b
Tomasz Grodzki <tomasz@alphasoc.com>
2018-06-15 16:47:58 +0200

    Make it compile on Windows

--- alerts/syslog_writer_windows.go
+++ alerts/syslog_writer_windows.go
@@ -1,40 +1,25 @@
-// +build !windows,!nacl,!plan9
-
 package alerts
 
-import (
-	"encoding/json"
-	"fmt"
-	"log/syslog"
-)
+import "errors"
+
+var errSyslogNotImplemented = errors.New("syslog not implemented for this platform")
 
 // SyslogWriter implements Writer interface and write
 // api alerts to syslog server.
 type SyslogWriter struct {
-	w *syslog.Writer
 }
 
 // NewSyslogWriter creates new syslog writer.
 func NewSyslogWriter(raddr string) (*SyslogWriter, error) {
-	w, err := syslog.Dial("tcp", raddr, logalert, tag)
-	if err != nil {
-		return nil, fmt.Errorf("connect to syslog input failed: %s", err)
-	}
-
-	return &SyslogWriter{w: w}, nil
+	return nil, errSyslogNotImplemented
 }
 
 // Write writes alert response to the syslog input.
 func (w *SyslogWriter) Write(alert *Alert) error {
-	b, err := json.Marshal(alert)
-	if err != nil {
-		return err
-	}
-
-	return w.w.Alert(string(b))
+	return errSyslogNotImplemented
 }
 
 // Close closes a connecion to the syslog server.
 func (w *SyslogWriter) Close() error {
-	return w.w.Close()
+	return errSyslogNotImplemented
 }
