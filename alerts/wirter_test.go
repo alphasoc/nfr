5110d8e25891ebf5a43b9b6e1734c2b450e224c4
Tomasz Grodzki <tomasz@alphasoc.com>
2018-09-11 17:44:14 +0200

    Fix unit tests

--- alerts/wirter_test.go
+++ alerts/wirter_test.go
@@ -3,27 +3,39 @@ package alerts
 import (
 	"fmt"
 	"net"
+	"sort"
+	"strings"
 	"time"
 )
+
+func bytesToSortedStrings(bs [][]byte) []string {
+	s := make([]string, len(bs))
+	for n := range bs {
+		s[n] = string(bs[n])
+	}
+	sort.Strings(s)
+	return s
+}
 
 func ExampleFormatterCEF_dns() {
 	f := NewFormatterCEF()
 
-	b, err := f.Format(&Event{
-		Type:   "dns",
-		Flags:  []string{"c2", "young_domain"},
-		Groups: []Group{Group{Label: "boston"}},
+	bs, err := f.Format(&Event{
+		Type:      "alert",
+		EventType: "dns",
+		Flags:     []string{"c2", "young_domain"},
+		Groups:    []Group{Group{Label: "boston"}},
 		Threats: map[string]Threat{
 			"c2_comm": Threat{
 				Severity:    5,
 				Description: "C2 communication",
 			},
-			"nevermind": Threat{
-				Severity:    1,
-				Description: "not important",
+			"interesting": Threat{
+				Severity:    2,
+				Description: "Interesting event",
 			},
 		},
-		Timestamp:  time.Unix(1536242944, 123e6),
+		Timestamp:  time.Unix(1536242944, 123e6).UTC(),
 		SrcIP:      net.IPv4(1, 2, 3, 4),
 		Query:      "virus.com",
 		RecordType: "A",
@@ -33,29 +45,32 @@ func ExampleFormatterCEF_dns() {
 		panic(err)
 	}
 
-	fmt.Print(string(b))
+	fmt.Print(strings.Join(bytesToSortedStrings(bs), "\n"))
 
-	// Output: CEF:0|AlphaSOC|NFR|0.0.0|c2_comm|C2 communication|10|app=dns start=Sep 06 2018 16:09:04.123 CEST src=1.2.3.4 cs1=c2,young_domain cs2=boston query=virus.com requestMethod=A
+	// Output:
+	// CEF:0|AlphaSOC|NFR|0.0.0|c2_comm|C2 communication|10|app=dns rt=Sep 06 2018 14:09:04.123 UTC src=1.2.3.4 cs1=c2,young_domain cs1Label=flags cs2=boston cs2Label=groups query=virus.com requestMethod=A
+	// CEF:0|AlphaSOC|NFR|0.0.0|interesting|Interesting event|4|app=dns rt=Sep 06 2018 14:09:04.123 UTC src=1.2.3.4 cs1=c2,young_domain cs1Label=flags cs2=boston cs2Label=groups query=virus.com requestMethod=A
 }
 
 func ExampleFormatterCEF_ip() {
 	f := NewFormatterCEF()
 
-	b, err := f.Format(&Event{
-		Type:   "ip",
-		Flags:  []string{"c2", "young_domain"},
-		Groups: []Group{Group{Label: "boston"}},
+	bs, err := f.Format(&Event{
+		Type:      "alert",
+		EventType: "ip",
+		Flags:     []string{"c2", "young_domain"},
+		Groups:    []Group{Group{Label: "boston"}},
 		Threats: map[string]Threat{
 			"c2_comm": Threat{
 				Severity:    5,
 				Description: "C2 communication",
 			},
-			"nevermind": Threat{
-				Severity:    1,
-				Description: "not important",
+			"interesting": Threat{
+				Severity:    2,
+				Description: "Interesting event",
 			},
 		},
-		Timestamp: time.Unix(1536242944, 123e6),
+		Timestamp: time.Unix(1536242944, 123e6).UTC(),
 		SrcIP:     net.IPv4(1, 2, 3, 4),
 		SrcPort:   16830,
 		DstIP:     net.IPv4(4, 3, 2, 1),
@@ -69,7 +84,9 @@ func ExampleFormatterCEF_ip() {
 		panic(err)
 	}
 
-	fmt.Print(string(b))
+	fmt.Print(strings.Join(bytesToSortedStrings(bs), "\n"))
 
-	// Output: CEF:0|AlphaSOC|NFR|0.0.0|c2_comm|C2 communication|10|app=ip start=Sep 06 2018 16:09:04.123 CEST src=1.2.3.4 cs1=c2,young_domain cs2=boston spt=16830 dst=4.3.2.1 dpt=443 proto=tcp in=744 out=1376
+	// Output:
+	// CEF:0|AlphaSOC|NFR|0.0.0|c2_comm|C2 communication|10|app=ip rt=Sep 06 2018 14:09:04.123 UTC src=1.2.3.4 cs1=c2,young_domain cs1Label=flags cs2=boston cs2Label=groups spt=16830 dst=4.3.2.1 dpt=443 proto=tcp in=744 out=1376
+	// CEF:0|AlphaSOC|NFR|0.0.0|interesting|Interesting event|4|app=ip rt=Sep 06 2018 14:09:04.123 UTC src=1.2.3.4 cs1=c2,young_domain cs1Label=flags cs2=boston cs2Label=groups spt=16830 dst=4.3.2.1 dpt=443 proto=tcp in=744 out=1376
 }
